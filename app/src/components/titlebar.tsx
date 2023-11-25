776350795d773ebdcf9dd73964aa7f5d2e354d39
Inbar Barkai <inbarbarkai@gmail.com>
2023-11-25 12:06:38 +0200

    Add basic infrastructure

---
+++ app/src/components/titlebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TitleBar = () => {
+  return (
+    <header className="title-bar">
+      <h1>Maagan Michal Portal</h1>
+    </header>
+  );
+};
+
+export default TitleBar;
